d11f7ba84226449ca92037f43c86bf701791093e
DR Dev <dromeroldev@gmail.com>
2024-10-31 22:46:55 -0500

    Get mechanic by id

--- routes/mechanics.js
+++ routes/mechanics.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createMechanic, listMechanics, testMechanic } from "../controllers/mechanic.js";
+import { createMechanic, getMechanic, listMechanics, testMechanic } from "../controllers/mechanic.js";
 
 const router = Router();
 
 router.get('/test-mechanic', testMechanic);
 router.post('/create-mechanic', createMechanic);
 router.get('/list-mechanics/:page?', listMechanics);
+router.get('/get-mechanics/:id', getMechanic);
 
 export default router;
