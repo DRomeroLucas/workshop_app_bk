fb4a66d61c11e8d96151025ede0bf4a64fdec330
spartanis119 <camargobarajasdanielfelipe72@gmail.com>
2024-11-03 02:57:14 +0000

    Adding method for list clients that aren't deleted

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 // Importart express
 import { Router } from "express";
 // Importar metodos del controlador a usar
-import { register, login, profile, softDelete, listUsers } from "../controllers/user.js";
+import { register, login, profile, softDelete, listUsers, listClients } from "../controllers/user.js";
 import { ensureAuth } from "../middlewares/auth.js";
 
 const router = Router();
@@ -10,6 +10,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/profile/:id', ensureAuth, profile);
 router.get('/listUsers/:page?', ensureAuth, listUsers);
+router.get('/listClients/:page?', ensureAuth, listClients);
 router.patch('/delete', ensureAuth, softDelete);
 
 export default router;  
