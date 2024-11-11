51add9ee7c27239f1e3c912f901b1d209ebb4a5f
spartanis119 <camargobarajasdanielfelipe72@gmail.com>
2024-11-11 20:48:49 +0000

    updating create and update method

--- routes/services.js
+++ routes/services.js
@@ -5,12 +5,12 @@ import { ensureAuth } from "../middlewares/auth.js";
 const router = Router();
 
 router.get('/test-service', testService);
-router.post('/create-service', createService);
+router.post('/create-service', ensureAuth, createService);
 router.get('/services', listServices);
 router.get('/service/:id', getService);
-router.put('/upgrade-service/:id', updateService);
-router.delete('/delete/:id', deleteService);
-router.delete('/permanent-erase/:id', hardDeleteService);
-router.put('/reactivate/:id', activateService);
+router.put('/upgrade-service/:idService', ensureAuth, updateService);
+router.delete('/delete/:id', ensureAuth, deleteService);
+router.delete('/permanent-erase/:id', ensureAuth, hardDeleteService);
+router.put('/reactivate/:id', ensureAuth, activateService);
 
 export default router;
